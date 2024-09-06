b23c3c0484445a577f81970ef703e1dc2d8edb7c
Joel <joel@10pines.com>
2024-09-06 13:13:26 -0300

    se agregan estilos para distintos tipos de mensajes

--- prueba-sockets-front/src/app/mensajes/mensajes.js
+++ prueba-sockets-front/src/app/mensajes/mensajes.js
@@ -3,6 +3,12 @@
 import styles from "./mensajes.module.css";
 import { useState } from "react";
 
+const tipoMensajes = {
+  COMUN: "COMUN",
+  BROADCAST: "BROADCAST",
+  PRIVADO: "PRIVADO",
+}
+
 const MensajeComun = ({ mensaje }) => {
   return (
     <p className={styles.mensajeComun}>
@@ -11,14 +17,51 @@ const MensajeComun = ({ mensaje }) => {
   );
 };
 
+const MensajeBroadcast = ({ mensaje }) => {
+  return (
+    <p className={styles.mensajeBroadcast}>
+      {mensaje}
+    </p>
+  );
+};
+
+const MensajePrivado = ({ mensaje }) => {
+  return (
+    <p className={styles.mensajePrivado}>
+      {mensaje}
+    </p>
+  );
+};
+
 export function Mensajes() {
   const [mensajes, setMensajes] = useState([
-    "[19.07.2016 08:42:28] <tyurderia> b a b a b ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab  ab ab ab",
-    "[19.07.2016 08:42:38] <tyurd> wazzup",
+    {
+      tipo: tipoMensajes.COMUN,
+      texto: "[19.07.2016 08:42:28] <tyurderia> b a b a b ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab  ab ab ab",
+    },
+    {
+      tipo: tipoMensajes.PRIVADO,
+      texto: "[19.07.2016 08:42:38] [Private] <tyurd> lellel",
+    },
+    {
+      tipo: tipoMensajes.COMUN,
+      texto: "[19.07.2016 08:42:38] <tyurd> wazzup",
+    },
+    {
+      tipo: tipoMensajes.BROADCAST,
+      texto: "[19.07.2016 08:42:38] <Server> Saludos de su servidor amigue!",
+    },
   ])
 
-  const listaMensajes = mensajes.map((mensaje, idx) =>
-    <MensajeComun mensaje={mensaje} key={idx}/>);
+  const listaMensajes = mensajes.map((mensaje, idx) => {
+    if(mensaje.tipo === tipoMensajes.COMUN) {
+      return <MensajeComun mensaje={mensaje.texto} key={idx}/>;
+    } else if(mensaje.tipo === tipoMensajes.BROADCAST) {
+      return <MensajeBroadcast mensaje={mensaje.texto} key={idx}/>;
+    } else if(mensaje.tipo === tipoMensajes.PRIVADO) {
+      return <MensajePrivado mensaje={mensaje.texto} key={idx}/>;
+    }
+  });
 
   return (
     <div className={styles.chat}>
